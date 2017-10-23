b303f2730b0d54718f603a8a58186bb793d52fa0
Artem Parfenenko <parfenens@gmail.com>
2017-10-23 16:08:33 +0300

    Created entities in inventory module: Order with list of order items, total price, customer mail, order date, payment sign, id, name and description fields

---
+++ inventory/src/main/java/com/netcracker/parfenenko/entities/Order.java
@@ -0,0 +1,84 @@
+package com.netcracker.parfenenko.entities;
+
+import com.netcracker.parfenenko.content.OrderItem;
+
+import java.util.List;
+
+public class Order {
+
+    private List<OrderItem> orderItems;
+    private int totalPrice;
+    private String customerMail;
+    private String orderDate;
+    private String paymentSign;
+    private long id;
+    private String name;
+    private String description;
+
+    public Order() {}
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getCustomerMail() {
+        return customerMail;
+    }
+
+    public void setCustomerMail(String customerMail) {
+        this.customerMail = customerMail;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getPaymentSign() {
+        return paymentSign;
+    }
+
+    public void setPaymentSign(String paymentSign) {
+        this.paymentSign = paymentSign;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
