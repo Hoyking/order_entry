cdc54e23adbd9667d49c5ff06c64035d083c0412
Artem Parfenenko <parfenens@gmail.com>
2017-12-06 21:07:39 +0300

    Fixed AppConfig [catalog]

--- catalog/src/main/java/com/netcracker/parfenenko/config/AppConfig.java
+++ catalog/src/main/java/com/netcracker/parfenenko/config/AppConfig.java
@@ -4,6 +4,11 @@ import com.netcracker.parfenenko.entities.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class AppConfig {
@@ -12,6 +17,20 @@ public class AppConfig {
     @Scope(value = "prototype")
     public Tag tag() {
         return new Tag();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.netcracker.parfenenko.controller"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
