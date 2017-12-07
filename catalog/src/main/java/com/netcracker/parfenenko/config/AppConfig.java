259722a2c258afe4823d3ce82dd97606c445a873
Artem Parfenenko <parfenens@gmail.com>
2017-12-07 15:30:49 +0300

    Separated swagger and application configurations

--- catalog/src/main/java/com/netcracker/parfenenko/config/AppConfig.java
+++ catalog/src/main/java/com/netcracker/parfenenko/config/AppConfig.java
@@ -4,11 +4,6 @@ import com.netcracker.parfenenko.entities.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.springframework.web.client.RestTemplate;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class AppConfig {
@@ -17,20 +12,6 @@ public class AppConfig {
     @Scope(value = "prototype")
     public Tag tag() {
         return new Tag();
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.netcracker.parfenenko.controller"))
-                .paths(PathSelectors.any())
-                .build();
     }
 
 }
