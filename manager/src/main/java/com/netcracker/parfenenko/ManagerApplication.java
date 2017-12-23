2575248694dfdd6b8261a6224444664d4c1384ee
Artem Parfenenko <parfenens@gmail.com>
2017-12-24 02:35:30 +0300

    Added service-discovery and api-gateway

--- manager/src/main/java/com/netcracker/parfenenko/ManagerApplication.java
+++ manager/src/main/java/com/netcracker/parfenenko/ManagerApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableEurekaClient
 public class ManagerApplication {
 
     public static void main(String[] args) {
